3c4a5d884c2b4779fb0f371a3f0e10b1128037cc
Herman918 <herman.bog@mail.ru>
2023-11-07 19:43:34 +0300

    third commit(Clean architecture)

--- app/src/main/java/com/example/constructionapp1/Presentation/FirstPageAfterLogin/SupervisorActivity.java
+++ app/src/main/java/com/example/constructionapp1/Presentation/FirstPageAfterLogin/SupervisorActivity.java
@@ -24,10 +24,10 @@ public class SupervisorActivity extends AppCompatActivity {
         setContentView(R.layout.afterlogin_thislayout_specific_for_supervisor);
 
         ArrayList<Card> cardTile = new ArrayList<>();
-        cardTile.add(new Card("LaborActivity", R.drawable.laborcopy, Color.parseColor("#fde0dc")));
-        cardTile.add(new Card("EquipmentActivity", R.drawable.equipmentcopy, Color.parseColor("#a6baff")));
+        cardTile.add(new Card("Labor", R.drawable.laborcopy, Color.parseColor("#fde0dc")));
+        cardTile.add(new Card("Equipment", R.drawable.equipmentcopy, Color.parseColor("#a6baff")));
         cardTile.add(new Card("Tasks", R.drawable.taskcopy, Color.parseColor("#42bd41")));
-        cardTile.add(new Card("RequirementActivity", R.drawable.requirementcopy, Color.parseColor("#fdd835")));
+        cardTile.add(new Card("Requirement", R.drawable.requirementcopy, Color.parseColor("#fdd835")));
         cardTile.add(new Card("Report", R.drawable.reportcardcopy, Color.parseColor("#90a4ae")));
         firstpageadapter adapter = new firstpageadapter(this, cardTile);
         GridView gridView = findViewById(R.id.firstopening);
